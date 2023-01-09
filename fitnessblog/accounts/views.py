3ffb605d093704e2f92343f326a53ff66a276dc5
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-09 14:02:05 +0000

    Added login functionality for both the login and signup view

--- fitnessblog/accounts/views.py
+++ fitnessblog/accounts/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
-def signup(request):
+def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # Log user in
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # Log user in
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+    
