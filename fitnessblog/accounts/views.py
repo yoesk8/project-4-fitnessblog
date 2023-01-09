ffe9d61007fb29c64010c7fdf3ed9c5a2134d7d5
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-09 12:42:36 +0000

    Added signup functionality

--- fitnessblog/accounts/views.py
+++ fitnessblog/accounts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Log user in
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
-
-
-
