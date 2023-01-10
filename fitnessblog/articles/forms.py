1e5e92d4eed314b9f881a6ec7050f7b8612639dd
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-10 11:07:43 +0000

    Added author field to articles form and logic to only allow users to create articles under their own username

---
+++ fitnessblog/articles/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from . import models
+
+
+class CreateArticle(forms.ModelForm):
+    class Meta:
+        model = models.Article
+        fields = [
+            'title',
+            'body',
+            'slug',
+            'thumb'
+        ]
