8c34aaf150776f75e2bd77294735e5ac4731e13f
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-16 23:46:26 +0000

    Replaced secret key

--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@pna6^h%52df@)^pw^sh7no(@p59g0^)wo)d8&a@sk%o3$$6iy'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://fitnessblog.herokuapp.com/', 'localhost']
 
