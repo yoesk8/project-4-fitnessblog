d0f1f81fe7034b3f51c3195dc2ef299551e977bb
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-16 23:07:11 +0000

    Modified settings

--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -30,6 +30,22 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
 # Application definition
 
