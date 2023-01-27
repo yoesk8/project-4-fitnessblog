e1aac6632956bad9f8a4b58a5f76e6943cd4dbf3
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-27 18:43:29 +0000

    Changed Debug to false in production

--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -38,8 +38,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 
 ALLOWED_HOSTS = ['*']
