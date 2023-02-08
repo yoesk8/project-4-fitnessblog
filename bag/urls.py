34002c6982fa4d726b6c0211f60dd8b8f82b20bc
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-02-08 12:26:05 +0000

    Created new products in the admin and created add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'bag'
+
+
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
