a25072157b9f8a97b70e1d6d71e11e5397b3157a
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-02-11 18:32:03 +0000

    Added toasts functionality and basic structure

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+from products.models import Product
 
 @login_required
 def view_bag(request):
@@ -13,6 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -21,6 +24,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     print(request.session['bag'])
@@ -44,6 +48,7 @@ def adjust_bag(request, item_id):
 
 @login_required
 def remove_from_bag(request, item_id):
+    """ Remove items from bag """
     bag = request.session.get('bag', {})
     try:
         bag.pop(item_id)
--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -191,3 +191,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ['https://8000-yoesk8-project4fitnessb-602pm453w1p.ws-eu86.gitpod.io']
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
