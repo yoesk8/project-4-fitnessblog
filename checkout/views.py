d124d67c27f99881b62e5a19d2f62c4746c2f4f1
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-02-13 15:11:15 +0000

    Added stripe credit card skeleton to the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MU89QG0etn7Vxsoho7woZ7qHhsSBphjXkxoqb6NZoWfaglAF9B1U6eeYhJUo4nBSyuoS4IHdOrI2mj6gjjeUo7W00P4Q643zl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
