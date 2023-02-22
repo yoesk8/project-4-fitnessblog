d6ca5bb934f839c57b5ce5664b3b40c83f84fd9d
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-02-22 12:28:57 +0000

    Changed static files settings

--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -156,12 +156,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
-django_heroku.settings(locals())
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
